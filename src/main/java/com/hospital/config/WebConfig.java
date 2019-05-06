52b210ae82245e4291db73b92547b1fbbd7c46fc
niujiatao <niujiatao@tongyu.tech>
2019-05-06 15:32:19 +0800

    添加登陆拦截器

---
+++ src/main/java/com/hospital/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.hospital.config;
+import com.hospital.config.intercepors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+ 
+  // 这个方法是用来配置静态资源的，比如html，js，css，等等
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+  }
+ 
+  // 这个方法用来注册拦截器，我们自己写好的拦截器需要通过这里添加注册才能生效
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+      registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/hospital/login","/hospital", "/regest","/patient/search");
+  }
+}
+
+
+
